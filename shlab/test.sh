c0fa405ef13f3382ed17fbb3cd68f001853525a5
kk0in <84329238+kk0in@users.noreply.github.com>
2022-04-12 18:12:11 +0900

    Add files via upload

---
+++ shlab/test.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+make test01 >> test.txt
+make test02 >> test.txt
+make test03 >> test.txt
+make test04 >> test.txt
+make test05 >> test.txt
+make test06 >> test.txt
+make test07 >> test.txt
+make test08 >> test.txt
+make test09 >> test.txt
+make test10 >> test.txt
+make test11 >> test.txt
+make test12 >> test.txt
+make test13 >> test.txt
+make test14 >> test.txt
+make test15 >> test.txt
+make test16 >> test.txt
+
+
+make rtest01 >> rtest.txt
+make rtest02 >> rtest.txt
+make rtest03 >> rtest.txt
+make rtest04 >> rtest.txt
+make rtest05 >> rtest.txt
+make rtest06 >> rtest.txt
+make rtest07 >> rtest.txt
+make rtest08 >> rtest.txt
+make rtest09 >> rtest.txt
+make rtest10 >> rtest.txt
+make rtest11 >> rtest.txt
+make rtest12 >> rtest.txt
+make rtest13 >> rtest.txt
+make rtest14 >> rtest.txt
+make rtest15 >> rtest.txt
+make rtest16 >> rtest.txt
+
+
